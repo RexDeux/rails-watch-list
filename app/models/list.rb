4ffc32b2c8ea172bd951d7684c141be320749bc8
Nuno Costa <nm.cdc@hotmail.com>
2021-11-21 21:03:13 +0000

    fixed

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
   has_many :bookmarks,
-  has_many :movie, through :bookmark
+  has_many :movies, through :bookmark
   has_many :reviews
 
   validates :name, uniqueness: true, presence: true
