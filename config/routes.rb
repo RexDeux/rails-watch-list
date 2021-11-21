362fb005c5aa4d23071f2d893ecd7766aa2e2c6b
Nuno Costa <nm.cdc@hotmail.com>
2021-11-21 22:52:21 +0000

    fixed some errors

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'lists#index'
   resources :lists do
     resources :bookmarks, only: [:new, :create]
-      «resources :bookmarks, only: :create
+    resources :bookmarks, only: :create
   end
   resources :bookmarks, only: :destroy
   resources :reviews, only: :destroy
